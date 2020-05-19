c0a8d95e56c26ae300fb9d51b72e59d3c00d8056
Tierry Ray <tierry.machado@agilecontent.com>
2020-05-19 18:33:25 -0300

    first commit

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,14 @@
+import { Ingredient } from '../shared/ingredient.model';
+export class Recipe {
+  constructor(
+    public name: string,
+    public description: string,
+    public imagePath: string,
+    public ingredients: Ingredient[]
+  ) {
+    this.name = name;
+    this.description = description;
+    this.imagePath = imagePath;
+    this.ingredients = ingredients;
+  }
+}
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,27 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super tasty Schnitzel - just awesome!',
+      'https://cdn.guiadacozinha.com.br/wp-content/uploads/2020/01/doritos-caseiro-1-1.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+    ),
+  ];
+
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+}
